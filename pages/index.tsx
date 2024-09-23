4630e92e823aece8b65dedd708528ffb07bf7d35
omitchen <omitchen@gmail.com>
2024-09-23 18:11:06 +0800

    feat(UI): update upload box UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
-import Image from "next/image";
-import localFont from "next/font/local";
+// import Image from "next/image";
+// import localFont from "next/font/local";
 import NoSSRWrapper from "@/components/csr";
 import VideoCompressor from "@/components/VideoCompressor";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export default function Home() {
   return (
