b4899d48dc2559c5b1b18f697e7ec69a9e60350b
omitchen <omitchen@gmail.com>
2024-09-24 17:05:06 +0800

    feat(page): update page

--- components/VideoCompressor/index.tsx
+++ components/VideoCompressor/index.tsx
@@ -1,24 +1,22 @@
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { fetchFile, toBlobURL } from "@ffmpeg/util";
 import { useEffect, useRef, useState } from "react";
-import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
 import FileUpload from "./FileUpload";
 
 export default function Component() {
   const [isLoading, setIsLoading] = useState(true);
   const ffmpegRef = useRef(new FFmpeg());
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  const messageRef = useRef<HTMLParagraphElement | null>(null);
   const [progress, setProgress] = useState(0);
   const [outputFileName, setOutputFileName] = useState("");
+  const [isCompressing, setIsCompressing] = useState(false);
+  const [compressionRatio, setCompressionRatio] = useState<number | null>(null);
 
   const load = async () => {
     setIsLoading(true);
     const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd";
     const ffmpeg = ffmpegRef.current;
-    ffmpeg.on("log", ({ message }) => {
-      if (messageRef.current) messageRef.current.innerHTML = message;
-    });
     await ffmpeg.load({
       coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
       wasmURL: await toBlobURL(
@@ -54,9 +52,17 @@ export default function Component() {
     const inputWidth = resolutionMatch ? parseInt(resolutionMatch[1]) : 0;
     const inputBitrate = bitrateMatch ? parseInt(bitrateMatch[1]) : 0;
 
+    console.log("====", {
+      inputWidth,
+      inputBitrate,
+      inputFileName,
+      outputFileName,
+      resolutionMatch,
+      bitrateMatch,
+    });
+
     // 根据输入视频的特征决定压缩参数
     let compressionParams = [];
-    console.log("====", inputWidth > 1280 || inputBitrate > 2000);
     if (inputWidth > 1280 || inputBitrate > 2000) {
       compressionParams = [
         "-i",
@@ -64,59 +70,73 @@ export default function Component() {
         "-c:v",
         "libx264",
         "-crf",
-        "23",
+        "28",
         "-preset",
-        "medium",
+        "veryfast",
         "-c:a",
         "aac",
         "-b:a",
-        "128k",
+        "96k",
         "-movflags",
         "+faststart",
         "-f",
-        "mp4",
+        fileExtension,
       ];
     } else {
-      // 如果视频已经很小或质量较低，仅进行轻微压缩
+      // 对于较小或质量较低的视频，也稍微增加压缩力度
       compressionParams = [
+        "-i",
+        inputFileName,
         "-c:v",
         "libx264",
         "-crf",
-        "18",
+        "23", // 增加CRF值
         "-preset",
-        "fast",
+        "veryfast", // 使用更快的预设
         "-c:a",
-        "copy",
+        "aac",
+        "-b:a",
+        "96k", // 降低音频比特率
+        "-f",
+        fileExtension,
       ];
     }
+    ffmpeg.on("progress", ({ progress }) => {
+      setProgress(Math.round(progress * 100));
+    });
 
-    // 执行压缩
     await ffmpeg.exec([...compressionParams, outputFileName]);
-
-    // 读取压缩后的文件
     const data = await ffmpeg.readFile(outputFileName);
 
-    // 更新视频源
+    const originalSize = file.size;
+    const compressedSize = data.length;
+    const ratio = (1 - compressedSize / originalSize) * 100;
+    setCompressionRatio(parseFloat(ratio.toFixed(2)));
+
     if (videoRef.current) {
       const blob = new Blob([data], { type: `video/${fileExtension}` });
       if (blob.size > 0) {
         const url = URL.createObjectURL(blob);
         videoRef.current.src = url;
       } else {
-        console.error("压缩后的视频文件大小为零");
+        console.error("compressed video file size is zero");
       }
     }
-
-    // 重置进度
-    setProgress(0);
   };
 
   const handleFileChange = async (file: File) => {
     setOutputFileName("");
+    setCompressionRatio(null);
+    setProgress(0);
     if (file) {
-      transcode(file).catch((err) => {
-        console.error("err", err);
-      });
+      setIsCompressing(true);
+      transcode(file)
+        .catch((err) => {
+          console.error("err", err);
+        })
+        .finally(() => {
+          setIsCompressing(false);
+        });
     }
   };
 
@@ -127,25 +147,42 @@ export default function Component() {
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-      {/* <input type="file" accept="video/*" onChange={handleFileChange} /> */}
-      <FileUpload onChange={handleFileChange} />
-      <p ref={messageRef}></p>
-      <br />
-      {progress > 0 && <p>处理进度: {progress}%</p>}
-      <video
-        ref={videoRef}
-        controls
-        onError={() => {
-          // 不支持播放直接触发下载逻辑
-          const a = document.createElement("a");
-          a.href = videoRef.current?.src || "";
-          a.download = outputFileName;
-          a.click();
-        }}
-        className="max-w-[500px] max-h-[500px]"
-      ></video>
-      <Button>压缩</Button>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="justify-center items-center flex flex-col overflow-y-auto gap-4 max-w-[1200px] w-full m-auto h-auto px-3 py-4">
+        {isCompressing ? (
+          <>
+            <Progress value={progress} />
+            <p className="leading-tight text-center">
+              Compressing: {progress}% <br />
+              <span className="text-red-400">{`(please don't close this page)`}</span>
+            </p>
+          </>
+        ) : (
+          <div className="md:w-[600px] w-full">
+            <FileUpload onChange={handleFileChange} />
+          </div>
+        )}
+        {compressionRatio !== null && (
+          <p className="leading-tight text-center">
+            compress ratio: {compressionRatio}%
+          </p>
+        )}
+        <video
+          ref={videoRef}
+          controls
+          loop
+          autoPlay
+          playsInline
+          muted
+          onError={() => {
+            const a = document.createElement("a");
+            a.href = videoRef.current?.src || "";
+            a.download = outputFileName;
+            a.click();
+          }}
+          className="max-w-[500px] max-h-[500px] w-full h-auto"
+        ></video>
+      </div>
     </div>
   );
 }
--- components/csr/index.tsx
+++ components/csr/index.tsx
@@ -1,9 +1,14 @@
 import dynamic from "next/dynamic";
-import React from "react";
+import React, { ReactNode } from "react";
 
-const NoSSRWrapper = (props) => (
+interface NoSSRWrapperProps {
+  children: ReactNode;
+}
+
+const NoSSRWrapper: React.FC<NoSSRWrapperProps> = (props) => (
   <React.Fragment>{props.children}</React.Fragment>
 );
+
 export default dynamic(() => Promise.resolve(NoSSRWrapper), {
   ssr: false,
 });
