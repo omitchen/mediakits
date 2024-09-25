d9643cd92c14ce312c1be54963d61ad165b6bad2
omitchen <omitchen@gmail.com>
2024-09-25 17:15:52 +0800

    feat(test): add http header

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
           },
           {
             key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
+            value: "credentialless",
           },
         ],
       },
