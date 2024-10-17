1227f5cae49bd5016e18e63eb8a6236ccc0cf506
omitchen <omitchen@gmail.com>
2024-10-17 16:20:39 +0800

    feat(landing): update landing

--- components/VideoCompressor/FileUpload.tsx
+++ components/VideoCompressor/FileUpload.tsx
@@ -10,10 +10,18 @@ export default function FileUpload({
 }) {
   const [files, setFiles] = useState<File>();
 
+  const handleFile = useCallback((file: File) => {
+    if (file.type.startsWith("video/")) {
+      setFiles(file);
+    } else {
+      toast.warning("Please upload a valid video file");
+    }
+  }, []);
+
   const onDrop = useCallback(
     (acceptedFiles: File[], fileRejections: FileRejection[]) => {
       if (acceptedFiles.length > 0) {
-        setFiles(acceptedFiles[0]);
+        handleFile(acceptedFiles[0]);
         return;
       }
 
@@ -21,7 +29,7 @@ export default function FileUpload({
         toast.warning("Please upload a valid video file");
       }
     },
-    []
+    [handleFile]
   );
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -38,6 +46,30 @@ export default function FileUpload({
     }
   }, [files, onChange]);
 
+  useEffect(() => {
+    const handleGlobalPaste = (event: ClipboardEvent) => {
+      const items = event.clipboardData?.items;
+      if (items) {
+        for (let i = 0; i < items.length; i++) {
+          if (items[i].type.indexOf("video") !== -1) {
+            const file = items[i].getAsFile();
+            if (file) {
+              handleFile(file);
+            }
+          } else {
+            toast.warning("Please upload a valid video file");
+          }
+          break;
+        }
+      }
+    };
+
+    document.addEventListener("paste", handleGlobalPaste);
+    return () => {
+      document.removeEventListener("paste", handleGlobalPaste);
+    };
+  }, [handleFile]);
+
   return (
     <div
       {...getRootProps()}
@@ -46,7 +78,9 @@ export default function FileUpload({
       <input {...getInputProps()} />
       <PlusIcon className="mx-auto h-12 w-12 text-gray-400" />
       <p className="mt-2 text-sm text-gray-500">
-        {isDragActive ? "Drop the file here ..." : "Upload or drop file here"}
+        {isDragActive
+          ? "Drop files here ..."
+          : "Upload, drag and drop, or paste files here"}
       </p>
     </div>
   );
--- components/VideoCompressor/landing.tsx
+++ components/VideoCompressor/landing.tsx
@@ -1,43 +1,165 @@
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import how1 from "@/assets/img/how1.png";
+import how2 from "@/assets/img/how2.png";
+import how3 from "@/assets/img/how3.png";
 
 const Landing = () => {
+  console.log(how1);
+
   return (
     <div className="flex flex-col gap-16 text-base mt-16">
-      <Card>
-        <CardHeader>
-          <CardTitle>How To Compress a Video?</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <ol className="list-decimal list-inside pl-4">
-            <li>
-              Click the “Upload or Drag file here” button to select your video
-              file
-            </li>
-            <li>
-              Keep the default options (they do a great job!) or specify
-              advanced options
-            </li>
-            <li>Click on the “Compress Video” button to start compression</li>
-            <li>
-              When the status change to “Done” click the “Download Video” button
-            </li>
-          </ol>
-        </CardContent>
-        <CardFooter>
-          <p>
-            World&apos;s best video compressor to compress{" "}
-            <span className="font-bold">MP4, AVI, MOV, WEBM, MKV </span>
-            file. Choose the default options to compress video size by 40%, or
-            choose a custom size.
+      <div className="flex flex-col gap-10 px-4 py-10 @container">
+        <div className="flex flex-col gap-4">
+          <h1 className=" tracking-ligh text-center text-[32px] font-bold leading-tight @[480px]:text-4xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em] max-w-[720px]">
+            Why mediakits?
+          </h1>
+          <p className=" text-base font-normal leading-normal max-w-full">
+            Make your videos smaller so they’re easier to share and store. No
+            watermarks, no file limits, no registration.
           </p>
-        </CardFooter>
-      </Card>
+        </div>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3 p-0">
+          <div className="flex flex-1 gap-3 rounded-lg border shadow-md  p-4 flex-col">
+            <div
+              className=""
+              data-icon="Clock"
+              data-size="24px"
+              data-weight="regular"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24px"
+                height="24px"
+                fill="currentColor"
+                viewBox="0 0 256 256"
+              >
+                <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm64-88a8,8,0,0,1-8,8H128a8,8,0,0,1-8-8V72a8,8,0,0,1,16,0v48h48A8,8,0,0,1,192,128Z"></path>
+              </svg>
+            </div>
+            <div className="flex flex-col gap-1">
+              <h2 className=" text-base font-bold leading-tight">
+                Quick compression
+              </h2>
+              <p className="text-[#999999] text-sm font-normal leading-normal">
+                Compress your videos in just a few clicks.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-1 gap-3 rounded-lg border shadow-md  p-4 flex-col">
+            <div
+              className=""
+              data-icon="ShieldCheck"
+              data-size="24px"
+              data-weight="regular"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24px"
+                height="24px"
+                fill="currentColor"
+                viewBox="0 0 256 256"
+              >
+                <path d="M208,40H48A16,16,0,0,0,32,56v58.78c0,89.61,75.82,119.34,91,124.39a15.53,15.53,0,0,0,10,0c15.2-5.05,91-34.78,91-124.39V56A16,16,0,0,0,208,40Zm0,74.79c0,78.42-66.35,104.62-80,109.18-13.53-4.51-80-30.69-80-109.18V56H208ZM82.34,141.66a8,8,0,0,1,11.32-11.32L112,148.68l50.34-50.34a8,8,0,0,1,11.32,11.32l-56,56a8,8,0,0,1-11.32,0Z"></path>
+              </svg>
+            </div>
+            <div className="flex flex-col gap-1">
+              <h2 className=" text-base font-bold leading-tight">
+                Privacy and security
+              </h2>
+              <p className="text-[#999999] text-sm font-normal leading-normal">
+                Your videos are private and secure. We don’t store your files in
+                anywhere.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-1 gap-3 rounded-lg border shadow-md  p-4 flex-col">
+            <div
+              className=""
+              data-icon="WifiHigh"
+              data-size="24px"
+              data-weight="regular"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24px"
+                height="24px"
+                fill="currentColor"
+                viewBox="0 0 256 256"
+              >
+                <path d="M140,204a12,12,0,1,1-12-12A12,12,0,0,1,140,204ZM237.08,87A172,172,0,0,0,18.92,87,8,8,0,0,0,29.08,99.37a156,156,0,0,1,197.84,0A8,8,0,0,0,237.08,87ZM205,122.77a124,124,0,0,0-153.94,0A8,8,0,0,0,61,135.31a108,108,0,0,1,134.06,0,8,8,0,0,0,11.24-1.3A8,8,0,0,0,205,122.77Zm-32.26,35.76a76.05,76.05,0,0,0-89.42,0,8,8,0,0,0,9.42,12.94,60,60,0,0,1,70.58,0,8,8,0,1,0,9.42-12.94Z"></path>
+              </svg>
+            </div>
+            <div className="flex flex-col gap-1">
+              <h2 className=" text-base font-bold leading-tight">
+                High definition
+              </h2>
+              <p className="text-[#999999] text-sm font-normal leading-normal">
+                Maintain high-definition video quality after compression.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-10 px-4 py-10">
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-4">
+            <h1 className="tracking-light text-center text-[32px] font-bold leading-tight @[480px]:text-4xl @[480px]:font-black @[480px]:leading-tight @[480px]:tracking-[-0.033em]">
+              How to compress a video on VidCompress
+            </h1>
+            <p className="text-base font-normal leading-normal max-w-full">
+              Make your videos smaller so they’re easier to share and store. No
+              watermarks, no file limits, no registration.
+            </p>
+          </div>
+          <Button
+            onClick={() => {
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+            }}
+            className="flex min-w-[84px] cursor-pointer max-w-[480px] items-center justify-center overflow-hidden rounded-xl h-10 px-4 @[480px]:h-12 @[480px]:px-5 bg-[#019863] hover:bg-[#019863]/90 text-[#FFFFFF] text-sm font-bold leading-normal tracking-[0.015em] @[480px]:text-base @[480px]:font-bold @[480px]:leading-normal @[480px]:tracking-[0.015em] w-fit"
+          >
+            <span className="truncate">Try it now</span>
+          </Button>
+        </div>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3">
+          <div className="flex flex-col gap-3 pb-3">
+            <div
+              className="w-full bg-center bg-no-repeat aspect-video bg-cover rounded-xl"
+              style={{
+                backgroundImage: `url(${how1.src})`,
+              }}
+            ></div>
+            <p className="text-[#FFFFFF] text-base font-medium leading-normal">
+              Upload a video
+            </p>
+          </div>
+          <div className="flex flex-col gap-3 pb-3">
+            <div
+              className="w-full bg-center bg-no-repeat aspect-video bg-cover rounded-xl"
+              style={{
+                backgroundImage: `url(${how2.src})`,
+              }}
+            ></div>
+            <p className="text-[#FFFFFF] text-base font-medium leading-normal">
+              Wait for the video to compress
+            </p>
+          </div>
+          <div className="flex flex-col gap-3 pb-3">
+            <div
+              className="w-full bg-center bg-no-repeat aspect-video bg-cover rounded-xl"
+              style={{
+                backgroundImage: `url(${how3.src})`,
+              }}
+            ></div>
+            <p className="text-[#FFFFFF] text-base font-medium leading-normal">
+              Download your compressed video
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
